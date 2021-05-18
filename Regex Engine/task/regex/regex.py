96b6bc95d237c86099f2d65eb1115b69b17d41a3
Filip <66736221+filipbartek6417@users.noreply.github.com>
2021-05-18 11:25:36 +0200

    Stage 1

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,12 +1,11 @@
 pair = input().split('|')
 match = 'True'
-if pair[0] != '':
-    for index, item in enumerate(pair[0]):
-        try:
-            if item != pair[1][index] and item != '.':
-                match = 'False'
-                break
-        except IndexError:
+for index, item in enumerate(pair[0]):
+    try:
+        if item != pair[1][index] and item != '.':
             match = 'False'
             break
+    except IndexError:
+        match = 'False'
+        break
 print(match)
